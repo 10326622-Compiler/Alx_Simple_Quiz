49101dcac6b6db4605fff5ff65bfe17f9ee93b25
Joseph <10326622@upsamail.edu.gh>
2025-12-01 02:02:28 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,29 @@
+// Arithmetic Functions
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+// Event Listeners for Operation Buttons
+
+// Addition
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+// Subtraction
+document.getElementById('subtract').addEventListener('c
